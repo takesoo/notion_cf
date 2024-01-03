9130984ab72dbc1c45df9bf439673907bac09f0b
takesoo <takesooon@gmail.com>
2024-01-03 19:09:28 +0900

    remove @page_id from template.rb

--- lib/notion_cf/cli.rb
+++ lib/notion_cf/cli.rb
@@ -10,7 +10,7 @@ module NotionCf
 
     desc 'deploy PAGE_ID TEMPLATE_FILE', 'Generate a new Notion page'
     def deploy(page_id, template_file)
-      blueprints = Template.build_from_file(page_id:, file: template_file).blueprints
+      blueprints = Template.build_from_file(file: template_file).blueprints
       NotionCf::Resources.new(page_id:).deploy(blueprints)
       generate(page_id)
     end
@@ -19,7 +19,7 @@ module NotionCf
     def generate(page_id)
       resources = NotionCf::Resources.new(page_id:)
       resources.retrieve
-      file_path = NotionCf::Template.new(page_id:, blueprints: resources.blueprints).create
+      file_path = NotionCf::Template.new(blueprints: resources.blueprints).create(page_id)
       puts "created #{file_path}"
     end
   end
--- lib/notion_cf/template.rb
+++ lib/notion_cf/template.rb
@@ -6,24 +6,23 @@ module NotionCf
     attr_reader :blueprints
 
     class << self
-      def build_from_file(page_id:, file:)
-        raise 'page_id is required' unless page_id
+      def build_from_file(file:)
         raise 'file is required' unless file
 
         string_key_hash = YAML.load_file("templates/#{file}")
         blueprints = JSON.parse(string_key_hash.to_json, symbolize_names: true)
-        new(page_id:, blueprints:)
+        new(blueprints:)
       end
     end
 
-    def initialize(page_id:, blueprints: [])
-      @page_id = page_id
+    def initialize(blueprints: [])
       @blueprints = blueprints
     end
 
-    def create
-      yaml = YAML.dump(blueprints).gsub(%r{!ruby/(\w|:)*}, '') # ハッシュをyamlに変換する際に出力される!ruby/hash:Notion::Blockみたいな文字列を削除する
-      file_path = "templates/#{@page_id}.yaml"
+    def create(page_id)
+      # ハッシュをyamlに変換する際に出力される!ruby/hash:Notion::Blockみたいな文字列を削除する
+      yaml = YAML.dump(blueprints).gsub(%r{!ruby/(\w|:)*}, '')
+      file_path = "templates/#{page_id}.yaml"
       File.open(file_path, 'w') { |file| file.write(yaml) }
       file_path
     end
